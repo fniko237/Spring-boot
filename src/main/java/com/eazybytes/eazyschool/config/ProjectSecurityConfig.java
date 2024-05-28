bf24f68c7833f770fe0bc17a9fdec0725d788b32
Dilane Azangue <dilane.azangue@pta.de>
2024-05-28 11:10:17 +0200

    Configuring Login and Logout pages

--- src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,17 +20,48 @@ public class ProjectSecurityConfig {
         //permit all Request inside the Web Application
         http
             .authorizeHttpRequests((requests) -> requests
+                    .requestMatchers("/dashboard").authenticated()
                     .requestMatchers("/home", "/", "").permitAll()
                     .requestMatchers("/holidays/**").permitAll()
                     .requestMatchers("/contact").permitAll()
                     .requestMatchers("/saveMsg").permitAll()
                     .requestMatchers("/courses").permitAll()
-                    .requestMatchers("/about").authenticated()
+                    .requestMatchers("/about").permitAll()
                     .requestMatchers("/assets/**").permitAll()
+                    .requestMatchers("/login").permitAll()
             )
-            .formLogin(withDefaults())
+            .formLogin(formLogin -> formLogin
+                    .loginPage("/login") // Specify the custom login page URL
+//                        .loginProcessingUrl("/home") // URL to submit the username and password to
+                    .defaultSuccessUrl("/dashboard", true) // URL to redirect to after a successful login
+                    .failureUrl("/login?error=true") // URL to redirect to after a failed login
+                    .permitAll() // Allow all users to see the login page
+            )
+            .logout(logout -> logout
+                    .logoutUrl("/logout") // URL to trigger logout
+                    .invalidateHttpSession(true) //Invalidate Http Session
+                    .deleteCookies("JSESSIONID") // Invalidate session cookies
+                    .logoutSuccessUrl("/login?logout=true") // URL to redirect to after successful logout
+                    .permitAll()
+            )
             .httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userService(){
+        UserDetails user  = User.withDefaultPasswordEncoder()
+                .username("dilane")
+                .password("12345")
+                .roles("USER")
+                .build();
+
+        UserDetails admin  = User.withDefaultPasswordEncoder()
+                .username("azangue")
+                .password("12345")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
